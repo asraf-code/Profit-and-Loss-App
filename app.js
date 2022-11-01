e7ded0c5d6d0bcd642b46b80295958d0e2c11271
asraf-code <64000853+asraf-code@users.noreply.github.com>
2022-11-01 13:42:22 +0530

    edited appjs

---
+++ app.js
@@ -0,0 +1,45 @@
+const initialPrice = document.querySelector("#input-price");
+const currentPrice = document.querySelector("#current-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const submitBtn = document.querySelector("#submit-btn");
+
+const outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener('click', calculateProfitAndLoss);
+
+function calculateProfitAndLoss() {
+    const ip = initialPrice.value;
+    const qty = stocksQuantity.value;
+    const curr = currentPrice.value;
+    if(ip&&qty&&curr) {
+        if(ip > curr) {
+            calculateLossValues(ip, curr, qty);
+        } else if (curr > ip){
+            calculateProfitValues(ip, curr, qty);
+        } else {
+            showOutput("No pain no gain and no gain no pain")
+        }
+    } else {
+        showOutput("Please enter values to calculate!")
+    }
+}
+
+function calculateLossValues(initial, current, quantity) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss/initial) * 100;
+
+    showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage.toFixed(2)}%`);
+
+}
+
+function calculateProfitValues(initial, current, quantity) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit/initial) * 100;
+
+    showOutput(`Hey the loss is ${profit} and the percent is ${profitPercentage.toFixed(2)}%`);
+
+}
+
+function showOutput (message) {
+    outputBox.innerHTML = message;
+}
